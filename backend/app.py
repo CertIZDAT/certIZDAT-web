36185dfc8311425266fdb15c4e967a5e030be1bd
sigseg5 <sigseg5@protonmail.com>
2023-03-22 13:18:59 +0400

    Add dump downloader to backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template
+import os
 
-app = Flask(__name__, template_folder='../frontend/', static_folder='../frontend/', static_url_path='/frontend')
+from flask import Flask, Response, render_template, send_file
+
+app = Flask(__name__, template_folder='../frontend/',
+            static_folder='../frontend/', static_url_path='')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/download_dump')
+def download_dump():
+    file_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '..',
+        'analyser',
+        'statistics.db')
+    return send_file(file_path, as_attachment=True, mimetype='application/x-sqlite3')
+
+
+if __name__ == '__main__':
+    app.run()
